b927f4f30c67caf9095ac6a776c584154798680a
Himanshi Latwal <you@example.com>
2025-12-09 13:29:00 +0530

    Added simple test workflow

---
+++ .github/workflows/test.js
@@ -0,0 +1,11 @@
+console.log("Running quick test...");
+let x = 10;
+let y = 10;
+
+if (x === y) {
+    console.log("Test Passed!");
+    process.exit(0);
+} else {
+    console.log("Test Failed!");
+    process.exit(1);
+}
