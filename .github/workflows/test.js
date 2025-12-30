728af39297ac536c821e28ac1d69b3f9894cfdcd
Himanshi Latwal <you@example.com>
2025-12-31 02:13:32 +0530

    Add code to trigger Sonar and Snyk checks

--- .github/workflows/test.js
+++ .github/workflows/test.js
@@ -9,3 +9,11 @@ if (x === y) {
     console.log("Test Failed!");
     process.exit(1);
 }
+function badFunction() {
+  var a = 10;
+  var a = 20;   // duplicate variable (code smell)
+  if (true) {
+    console.log("Always true"); // useless condition
+  }
+}
+badFunction();
